4b408f522cddf42e47728afd27d6000d303ea77d
Ankit Acharya <acharyaankit000@gmail.com>
2025-08-21 21:19:08 +0545

    Added the option to directly publish a post, or save as draft.

--- blog/views.py
+++ blog/views.py
@@ -37,12 +37,13 @@ def post_edit(request, pk):
         form = PostForm(request.POST, instance=post)
         post = form.save(commit=False)
         post.author = request.user
-        
+        if "publish" in request.POST:
+            post.published_date = timezone.now()    
         post.save()
         return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form':form})
+    return render(request, 'blog/post_edit.html', {'form':form, 'post':post})
     
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
